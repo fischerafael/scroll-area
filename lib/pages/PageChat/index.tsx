62b8abdc4c34601bbb11cd7e076290d4edf9e7c8
Rafael Fischer <rafaelsanfischer@gmail.com>
2024-10-03 18:41:13 -0300

    feat: made it scrollable

---
+++ lib/pages/PageChat/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export const PageChat = () => {
+  return (
+    <div className="h-[100vh] bg-pink-200 items-center flex flex-col py-4">
+      <div className="bg-red-100 w-full h-[80px] max-w-[1000px]">
+        <p>logo</p>
+      </div>
+
+      <div className="flex-1  bg-pink-300 w-full max-w-[1000px] relative">
+        <div className="bg-white absolute top-0 bottom-0 flex flex-col gap-4 overflow-y-auto w-full ">
+          {messages.map((m, index) => {
+            const isOdd = index % 2 === 1;
+            console.log(isOdd);
+            return (
+              <div
+                className={`bg-gray-100 h-fit p-8 max-w-[800px] ${
+                  isOdd ? "self-start" : "self-end"
+                }`}
+                key={index}
+              >
+                Existem 195 países no mundo atualmente, reconhecidos pela ONU.
+                Destes, 193 são membros das Nações Unidas, e 2 têm o status de
+                observadores: o Vaticano (Santa Sé) e a Palestina. Se quiser uma
+                lista específica ou de uma região, posso detalhar conforme
+                necessário!
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="bg-red-100 w-full h-[80px] max-w-[1000px]">
+        <p>logo</p>
+      </div>
+    </div>
+  );
+};
+
+const messages = [
+  "Existem 195 países no mundo atualmente, reconhecidos pela ONU. Destes, 193 são membros das Nações Unidas, e 2 têm o status de observadores: o Vaticano (Santa Sé) e a Palestina. Se quiser umab lista específica ou de uma região, posso detalhar conforme ecessário!",
+  "Existem 195 países no mundo atualmente, reconhecidos pela ONU. Destes, 193 são membros das Nações Unidas, e 2 têm o status de observadores: o Vaticano (Santa Sé) e a Palestina. Se quiser umab lista específica ou de uma região, posso detalhar conforme ecessário!",
+  "Existem 195 países no mundo atualmente, reconhecidos pela ONU. Destes, 193 são membros das Nações Unidas, e 2 têm o status de observadores: o Vaticano (Santa Sé) e a Palestina. Se quiser umab lista específica ou de uma região, posso detalhar conforme ecessário!",
+  "Existem 195 países no mundo atualmente, reconhecidos pela ONU. Destes, 193 são membros das Nações Unidas, e 2 têm o status de observadores: o Vaticano (Santa Sé) e a Palestina. Se quiser umab lista específica ou de uma região, posso detalhar conforme ecessário!",
+  "Existem 195 países no mundo atualmente, reconhecidos pela ONU. Destes, 193 são membros das Nações Unidas, e 2 têm o status de observadores: o Vaticano (Santa Sé) e a Palestina. Se quiser umab lista específica ou de uma região, posso detalhar conforme ecessário!",
+  "Existem 195 países no mundo atualmente, reconhecidos pela ONU. Destes, 193 são membros das Nações Unidas, e 2 têm o status de observadores: o Vaticano (Santa Sé) e a Palestina. Se quiser umab lista específica ou de uma região, posso detalhar conforme ecessário!",
+  "Existem 195 países no mundo atualmente, reconhecidos pela ONU. Destes, 193 são membros das Nações Unidas, e 2 têm o status de observadores: o Vaticano (Santa Sé) e a Palestina. Se quiser umab lista específica ou de uma região, posso detalhar conforme ecessário!",
+  "Existem 195 países no mundo atualmente, reconhecidos pela ONU. Destes, 193 são membros das Nações Unidas, e 2 têm o status de observadores: o Vaticano (Santa Sé) e a Palestina. Se quiser umab lista específica ou de uma região, posso detalhar conforme ecessário!",
+];
